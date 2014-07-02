16aea4afdd4de691edd66f615a7db6de4273d935
Duncan Doyle <Duncan.Doyle@gmail.com>
2014-07-02 14:52:41 +0200

    Improved the CustomEventPrintListener which now has a separate method to process each event.

--- InfinispanDemoClient/src/main/java/org/jboss/ddoyle/osc2013/infinispan/demo/client/listener/CustomEventPrintListener.java
+++ InfinispanDemoClient/src/main/java/org/jboss/ddoyle/osc2013/infinispan/demo/client/listener/CustomEventPrintListener.java
@@ -9,11 +9,8 @@ import org.infinispan.client.hotrod.annotation.ClientListener;
 import org.infinispan.client.hotrod.event.ClientCacheEntryCustomEvent;
 import org.jboss.ddoyle.osc2013.infinispan.demo.model.event.CustomEvent;
 
-
-
 @ClientListener(converterFactoryName = "static-converter")
 public class CustomEventPrintListener implements Serializable {
-	
 	
 	/**
 	 * SerialVersionUID.
@@ -21,24 +18,18 @@ public class CustomEventPrintListener implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@ClientCacheEntryCreated
-	@ClientCacheEntryModified
-	@ClientCacheEntryRemoved
 	public void handleCreatedEvent(ClientCacheEntryCustomEvent<CustomEvent> e) {
 		System.out.println("Created: " + e);
 	}
 	
+	@ClientCacheEntryModified
 	public void handleModifiedEvent(ClientCacheEntryCustomEvent<CustomEvent> e) {
 		System.out.println("Modified: " + e);
 	}
 	
-	public void handleDeletedEvent(ClientCacheEntryCustomEvent<CustomEvent> e) {
-		System.out.println("Deleted: " + e)
-	}
-	
-	/*
+	@ClientCacheEntryRemoved
 	public void handleRemovedEvent(ClientCacheEntryCustomEvent<CustomEvent> e) {
 		System.out.println("Removed: " + e);
 	}
-	*/
 	
 }
