0cce1e8fd9c53ce2cccfd9e411155d33b3e5a4b8
Duncan Doyle <Duncan.Doyle@gmail.com>
2014-11-04 16:30:50 +0100

    Major revision of the demo. Demo now runs in EC2, so uses S3PING. Introduced new functionality to demonstrate listening to RELAY2 events (JGroups). System also utilizes a new CustomFailurePolicy for XSite failure events. Renamed InfinispanHybridModeServerWeb to a new new name: GraftedMode.

--- InfinispanHybridModeServerWeb/src/main/java/org/jboss/ddoyle/osc2013/infinispan/demo/cache/CacheContainerFactory.java
+++
@@ -1,9 +0,0 @@
-package org.jboss.ddoyle.osc2013.infinispan.demo.cache;
-
-import org.infinispan.manager.CacheContainer;
-
-public interface CacheContainerFactory {
-
-	public abstract CacheContainer getCacheContainer();
-	
-}
